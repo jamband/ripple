daf5c04edf6a9669e79bcc775ebf1b4ed0d38121
Tomoki Morita <tmsongbooks215@gmail.com>
2017-03-14 17:39:49 +0900

    Update basic.php

--- samples/basic.php
+++ samples/basic.php
@@ -2,17 +2,13 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-// $url = 'https://folkadelphia.bandcamp.com/track/facing-west';
-// $url = 'https://soundcloud.com/the-staves/pay-us-no-mind';
-// $url = 'https://vimeo.com/67320034';
-$url = 'https://www.youtube.com/watch?v=MBlpfXLQLvU';
-
+$url = 'https://example.bandcamp.com/track/title';
 $ripple = new jamband\ripple\Ripple($url);
-var_dump($ripple->provider()); // YouTube
+var_dump($ripple->provider()); // Bandcamp
 var_dump($ripple->isValidUrl()); // true
 
 $ripple->request();
-var_dump($ripple->id()); // MBlpfXLQLvU
-var_dump($ripple->title()); // The Staves - The Motherlode (Official Video)
-var_dump($ripple->image()); // https://i.ytimg.com/vi/MBlpfXLQLvU/hqdefault.jpg
-var_dump($ripple->embed()); // https://www.youtube.com/embed/MBlpfXLQLvU
+var_dump($ripple->id()); // 123456789
+var_dump($ripple->title()); // Title, by Artist
+var_dump($ripple->image()); // https://img.example.com/img/1234567890.jpg
+var_dump($ripple->embed()); // https://bandcamp.com/EmbeddedPlayer/track=123456789/
