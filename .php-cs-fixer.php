7e069aaaedb84dd6f6f8a4003424e1e0a4a0cf1c
Tomoki Morita <tmsongbooks215@gmail.com>
2023-06-13 22:09:30 +0900

    improve php cs fixer config file

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
-return (new PhpCsFixer\Config())->setRules([
-    '@PSR12' => true,
-])->setFinder(PhpCsFixer\Finder::create()->in([
-    __DIR__.'/src',
-    __DIR__.'/tests',
-]));
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'declare_strict_types' => true,
+    ])->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+    );
